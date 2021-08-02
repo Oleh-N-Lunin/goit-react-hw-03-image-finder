a513c43787a443e3936b584261094d444d4841da
Oleh-N-Lunin <67375261+Oleh-N-Lunin@users.noreply.github.com>
2021-08-02 17:53:21 +0300

    Create Fetch API

---
+++ src/servises/FetchAPI.js
@@ -0,0 +1,20 @@
+function fetchImage(imageName, page) {
+    const KEY = "21886908-2dadc690508cf849d5de61581";
+    const LINK = "https://pixabay.com/api/";
+
+    return fetch(
+        `${LINK}?${KEY}&q=${imageName}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`
+    ).then((req) => {
+        if (req.ok) {
+            return req.json();
+        }
+
+        return Promise.reject(new Error("Don't have image"));
+    });
+}
+
+const api = {
+    fetchImage,
+};
+
+export default api;
